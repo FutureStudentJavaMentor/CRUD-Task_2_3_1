e6e131f7a8401146f1c87f46d1c8555becd90d79
Katanes! <nikitosv1@gmail.com>
2021-12-12 08:35:46 +0300

    Delete comment

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -50,16 +50,6 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
         ServletContext.addFilter("hiddenHttpMethodFilter",
                 new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null,true, "/*");
     }
-//    private void registerCharacterEncodingFilter(ServletContext aContext) {
-//        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
-//
-//        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-//        characterEncodingFilter.setEncoding("UTF-8");
-//        characterEncodingFilter.setForceEncoding(true);
-//
-//        FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
-//        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
-//
-//    }
+
 
 }
